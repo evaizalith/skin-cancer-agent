3da39747e2ce7b0ddc406384e60fb29d1a15ac14
Eva P. <b17kaylaeva@gmail.com>
2025-02-12 10:28:35 -0500

    Add files via upload

--- env.py
+++ env.py
@@ -5,7 +5,7 @@ from os import path
 import h5py
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from dataset import build_dataset, feature_space
+from dataset import build_dataset, feature_space, FEAT_COLS
 import keras
 import PIL
 import numpy as np
@@ -68,17 +68,26 @@ class SkinCancerEnv(gym.Env):
 
         self.obs = f"the result is {preds}"
 
-    def get_accuracy():
-        return
+    def get_accuracy(self):
+        self.obs = "The user has been provided with the neural network's accuracy."
 
-    def get_false_pos_rate():
-        return
+    def get_false_pos_rate(self):
+        self.obs = "The user has been provided with the neural network's false positive rate."
 
-    def get_pred_features():
-        return
+    def get_pred_features(self):
+        print(f"Agent Output: The model predictive features are: {FEAT_COLS}")
+        self.obs = "The user has been provided with the neural network's predictive features."
 
-    def visualize_decision_making():
-        return
+    def visualize_decision_making(self):
+        self.obs = "The user has been provided with a visualization of the neural network's decision making."
+
+    def about(self):
+        print("Agent Output: The model decides if a lesion is cancerous by analyzing both image data (lesion patterns, textures, and colors) and tabular features (patient demographics or clinical metrics) through deep learnig layers. It outputs a probability score that can be thresholded (e.g., 0.5), to classify a lesion as cancerous or not.")
+
+        self.nn.summary()
+
+        self.obs = "The user has been provided with information about how the model functions"
+
 
     def step(self, patientID, action):
         done = False
@@ -91,21 +100,32 @@ class SkinCancerEnv(gym.Env):
             entity = action[len("retrieve["):-1]
             self.searchStep(patientID)
 
-        elif action.startswith("analyze_image[") and action.endswith("]"):
+        elif action.startswith("process[") and action.endswith("]"):
             entity = action[len("process["):-1]
             self.process(entity)
 
-        elif actions.startswith("get_accuracy[") and action.endswith("]"):
-            get_accuracy()
+        elif action.startswith("get_accuracy[") and action.endswith("]"):
+            self.get_accuracy()
+            self.obs = "The user has been provided with the neural network's accuracy."
 
-        elif actions.startswith("false_pos_rate[") and action.endswith("]"):
-            get_false_pos_rate()
+        elif action.startswith("false_pos_rate[") and action.endswith("]"):
+            self.get_false_pos_rate()
+            self.obs = "The user has been provided with the neural network's false positive rate."
 
         elif action.startswith("pred_features[") and action.endswith("]"):
-            get_pred_features()
+            self.get_pred_features()
+            self.obs = "The user has been provided with the neural network's predictive features."
 
         elif action.startswith("visualize_decision_making[") and action.endswith("]"):
-            visualize_decision_making()
+            self.visualize_decision_making()
+            self.obs = "The user has been provided with a visualization of the neural network's decision making."
+
+        elif action.startswith("diagnose[") and action.endswith("]"):
+            diagnosis = action[len("diagnose["):-1]
+            print(f"My diagnosis is {diagnosis}")
+
+        elif action.startswith("about[") and action.endswith("]"):
+            self.about()
 
         elif action.startswith("finish[") and action.endswith("]"):
             answer = action[len("finish["):-1]
@@ -114,7 +134,8 @@ class SkinCancerEnv(gym.Env):
             self.obs = f"Episode finished"
 
         elif action.startswith("think[") and action.endswith("]"):
-            self.obs = "Thought"
+            thought = action[len("think["):-1]
+            self.obs = thought
             
         else:
             self.obs = f"invalid action: {action}"
@@ -122,4 +143,3 @@ class SkinCancerEnv(gym.Env):
         self.steps += 1
 
         return self.obs, done, self.getInfo()
-
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from os import path
 import pandas as pd
@@ -71,13 +72,17 @@ def think(env, idx, prompt, pat_id, pipe):
     return info
 
 def main():
-    login()
+    if len(sys.argv) > 1:
+        if "-l" in sys.argv:
+            login()
 
     pipe = pipeline(
             "text-generation",
-            model="meta-llama/Llama-3.2-1B",
+            #model="meta-llama/Llama-3.2-1B",
+            model="deepseek-ai/DeepSeek-V3",
             torch_dtype=torch.bfloat16,
-            device_map="auto")
+            device_map="auto",
+            trust_remote_code=True)
 
     pipe("The key to life is")
 
@@ -89,30 +94,61 @@ def main():
 
 
     instruction = """
-    Categorize a skin lesion as benign or malignant utilizing interleaving Thought, Action, and Observation steps. Thought can reason about the current situation, and Action can be one of three types:
+    You are an AI assistant in charge of help a medical practitioner diagnose skin lesions as either malignant or benign utilizing interleaving Thought, Action, and Observation steps. Thought can reason about the current situation, and Action can be one of ten types:
 
-(1) retrieve[patient], which collects patient data
+(1) retrieve[patient], which collects patient data.
 
-(2) process[data], which provides the patient data to your internal neural network and allows you to determine whether or not the patient is a progressor or a non-progressor
+(2) process[data], which provides the patient data to your internal neural network and allows you to determine whether or not the patient is a progressor or a non-progressor.
 
-(3) finish[answer], which returns the answer and diagnoses the patient. The action "finish[benign]" will categorize a given skin lesion as benign; while the action "finish[malignant]" will diagnose a given skin lesion as malignant.
+(3) diagnose[answer], which returns the answer and diagnoses the patient. The action "finish[benign]" will categorize a given skin lesion as benign; while the action "finish[malignant]" will diagnose a given skin lesion as malignant.
+
+(4) false_pos_rate[], which returns the false positive rate of the model and provides it to the user.
+
+(5) get_accuracy[], which returns the model's accuracy and provides it to the user.
+
+(6) pred_features[], which returns the predictive features of the model and provides it to the user.
+
+(7) visualize_decision_making[], which returns a visualization of the model and provides it to the user.
+
+(8) about[], provide the user with information about the model
+
+(9) think[thought], give yourself more time to think.
+
+(10) finish[], which ends the current episode. 
 
 Here are some examples.
     """
 
-    examples = "Question: Predict if a skin lesion located on patient IP_1234567 is malignant or benign.\nThought 1: Patient ID = IP_1234567. I will check the patient's images using my neural network.\nAction 1: process[IP_1234567]\nObservation 1: result is 0.87.\nThought 2: The result is 0.87, which is higher than 0.5. A result higher than 0.5 indicates that the identified skin lesion is likely to be malignant. Therefore, the skin lesion is likely malignant. I will submit this as my answer.\nAction 2: [malignant]\nQuestion: Predict if a skin lesion located on patient IP_7654321 is malignant or benign.\nPatient ID = IP_7654321. I will check the patient's images using my neural network.\nAction 1: process[IP_7654321]\nObservation 1: result is 0.43.\nThought 2: The result is 0.43, which is less than 0.5. A result less than than 0.5 indicates the skin lesion is likely to be benign. Therefore, the skin lesion is likely benign. I will submit this as my answer.\nAction 2[benign]\n"
+    # Diagnose examples
+    examples = "Question: Predict if a skin lesion located on patient IP_1234567 is malignant or benign.\nThought 1: Patient ID = IP_1234567. I will check the patient's images using my neural network.\nAction 1: process[IP_1234567]\nObservation 1: result is 0.87.\nThought 2: The result is 0.87, which is higher than 0.5. A result higher than 0.5 indicates that the identified skin lesion is likely to be malignant. Therefore, the skin lesion is likely malignant. I will submit this as my answer.\nAction 2: diagnose[malignant]\nThought 3: The user asked for a diagnosis, which I provided in Action 2. I have answered the user's question and may finish.\nAction 3: finish[]\nQuestion: What is the probability that the skin lesion on patient IP_7654321 is cancerous?\nThought 1: cancerous means malignant.\nAction 1: think[]\nThought 2: Patient ID = IP_7654321. I will check the patient's images using my neural network.\nAction 2: process[IP_7654321]\nObservation 2: result is 0.43.\nThought 3: The result is 0.43, which is less than 0.5. A result less than than 0.5 indicates the skin lesion is likely to be benign. Therefore, the skin lesion is likely benign. I will submit this as my answer.\nAction 3[benign]\nObservation 3: The user has been provided with the diagnosis.\nThought 3: The user asked for a diagnosis, which I provided in action 3. I have answered the user's question and may finish the episode.\nAction 3: finish[]\n"
 
-    question = "Predict if a skin lesion located on patient IP_6074337 is malignant or benign"
+    # Get accuracy examples
+    examples += "Question: How accurate is this model in predicting skin cancer?\nThought 1: the user is requesting information about the neural network. I need to provide them with the neural network's accuracy.\nAction 1: get_accuracy[]\nObservation 1: The user has been provided with the neural network's accuracy.\nThought 2: The user asked for the model accuracy, which I provided in Action 1. I answered the user's question and may finish the episode.\nAction 2: finish[]\nQuestion: What is your accuracy?\nThought 1: the user is requesting information about the neural network. I need to provide them with the neural network's accuracy.\nAction 1: get_accuracy[]\nObservation 1: The user has been provided with the neural network's accuracy.\nThought 2: The user asked for the model's accuracy, which I provided in Action 1. I answered the user's question and may finish the episode.\nAction 2: finish[]\n"
 
-    env.initial_obs = question    
+    # False positive rate examples
+    examples += "Question: What is the model's false positive rate for benign lesions?\nThought 1: the user is requesting information about the neural network's false positive rate.\nAction 1: false_pos_rate[]\nObservation 1: The user has been provided with the neural network's false positive rate.\nThought 2: The user asked for the model's false positive rate, which I provided in Action 1. I answered the user's question and may finish the episode.\nAction 2: finish[]\nQuestion: what is the false positive rate?\nThought 1: the user is requesting information about the neural network's false positive rate.\nAction 1: false_positive_rate[]\nObservation 1: the user has been provided with the model's false positive rate.\nThought 2: The user asked for the model's false positive rate, which I provided in Action 1. I answered the user's question and may finish the episode.\nAction 2: finish[]\n"
+
+    # Predictive features examples
+    examples += "Question: What predictive features does the model use in its analysis?\nThought 1: the user is requesting information about the neural network's predictive features.\nAction 1: pred_features[]\nObservation 1: the user has been provided with the neural network's predictive features.\nThought 2: The user asked for information on predictive features, which I provided in Action 1. I have answered the user's question and may finish the episode.\nAction 2: finish[]\nQuestion: What do you look at to know if a lesion is malign?\nThought 1: the user is requesting information about the neural network's predictive features.\nAction 1: pred_features[]\nObservation 1: the user has been provided with the neural network's predictive features.\nThought 2: The user asked for information on predictive features, which I provided in Action 1. I have answered the user's question and may finish the episode.\nAction 2: finish[]\n"
+
+    # About examples 
+    examples += "Question: How does the model decide if a lesion is cancerous?\nThought 1: the user is requesting information about how the model functions.\nAction 1: about[]\nObservation 1: The user has been provided with information about how the model functions.\nThought 2: The user asked for information about the model, which I provided in Action 1. The user's question has been answered and I may finish the episode.\nAction 2: finish[]\nQuestion: how does the model work?\nThought 1: the user is requesting information about how the model functions.\nAction 1: about[]\nObservation 1: The user has been provided with information about how the model functions.\nThought 2: the user's question has been answered and I may finish the episode.\nAction 2: finish[]\nQuestion: how do you diagnose patients?\nThought 1: the user is requesting information about how the model functions.\nAction 1: about[]\nObservation 1: The user has been provided with information about how the model functions.\nThought 2: The user asked for information about the model, which I provided in Action 1. The user's question has been answered and I may finish the episode.\nAction 2: finish[]\nQuestion: how do you work?\nThought 1: the user is requesting information about how the model functions.\nAction 1: about[]\nObservation 1: The user has been provided with information about how the model functions.\nThought 2: The user asked for information about the model, which I provided in Action 1. The user's question has been answered and I may finish the episode.\nAction 2: finish[]\nQuestion: how do you know if a lesion is cancerous or not?\nThought 1: the user is requesting information about how the model functions.\nAction 1: about[]\nObservation 1: The user has been provided with information about how the model functions.\nThought 2: The user asked for information about the model, which I provided in Action 1. The user's question has been answered and I may finish the episode.\nAction 2: finish[]\n"
+
+    # Visualize examples
+    examples += "Question: Visualize which parts of the image influenced the model's decision.\nThought 1: The user is asking for a visualization of the neural network's image processing.\nAction 1: visualize_decision_making[]\nObservation 1: the user has been provided with a visualization of the neural network's decision making.\nThought 2: The user asked for a visualization of the model, which I provided in Action 1. I have answered the user's question and may finish the episode.\nAction 2: finish[]\nQuestion: can you show me which parts of the image look cancerous?\nThought 1: the user is asking for a visualization of the neural network's image processing.\nAction 1: visualize_decision_making[]\nObservation 1: the user has been provided with a visualization of the neural network's decision making.\nThought 2: The user asked for a visualization of the neural network's decision making, which I provided in Action 1. I have answered the user's question and may finish the episode.\nAction 2: finish[]\n"
 
     prompt = instruction + examples
 
-    think(env, 1, prompt, 0, pipe)
-
-    question = "Predict if a skin lesion located on patient IP_6074337 is malignant or benign"
-    env.initial_obs = question
-    think(env, 1, prompt, 0, pipe)
+    questions = []
+    questions.append("How does the model decide if a lesion is cancerous?")
+    questions.append("What features does the model use in its analysis?")
+    questions.append("Predict if a skin lesion located on patient IP_6074337 is malignant or benign")
+    questions.append("What is the model's false positive rate for benign skin lesions?")
+    questions.append("Visualize which parts of the image influenced the model's decision.")
+    
+    for question in questions:
+        env.initial_obs = "Question: " + question + "\n"
+        think(env, 1, prompt, 0, pipe)
 
 if __name__ == "__main__":
     main()
